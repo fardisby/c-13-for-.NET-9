6f5b3140df0222c10415276ed91039ec10bcee81
fardiis <fardis.bahry.94>
2024-11-05 15:29:38 -0500

    Add project files.

---
+++ c# 13 for .NET 9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, C# 13");
+
+var subList = new DeveloperList("C#", ".NET 9");
+
+foreach (var sub in subList.Devs)   
+    Console.WriteLine(sub);
+
+public class DeveloperList
+{
+    public IList<string> Devs { get; private set; }
+
+    public DeveloperList(params List<string> devs)
+    {
+        Devs = devs;
+    }
+}

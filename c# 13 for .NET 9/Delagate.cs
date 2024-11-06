a0b7e6410c2909c903dd36e112192a892a7498c6
fardiis <bahryfardis@gmail.com>
2024-11-06 10:04:45 -0500

    adddelegate

---
+++ c# 13 for .NET 9/Delagate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__13_for_.NET_9
+{
+    public class Delagate
+    {
+        public int MyProperty { get; set; }
+    }
+}

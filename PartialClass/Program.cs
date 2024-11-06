9ddb5488f538de62333a155ab783e828d2c2da58
fardiis <bahryfardis@gmail.com>
2024-11-06 18:13:20 -0500

    partialclass

--- PartialClass/Program.cs
+++ PartialClass/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, C# 13");
+
+var voteCounter = new VoteCounter();
+
+voteCounter.Votes = 10;
+
+Console.WriteLine(voteCounter.Votes);
+
+public partial class VoteCounter
+{
+    public partial int Votes { get; set; }
+}
+
+public partial class VoteCounter
+{
+    private int _votes = 0;
+
+    public partial int Votes
+    {
+        get { return _votes; }
+        set { _votes = value; }
+    }
+}
